037ec904d7d342e43db2127f7a43f7ffe9205626
Olaf Janssen <o.t.a.janssen@gmail.com>
2015-04-30 21:21:34 +0200

    Catching getBBox exception on FireFox, fixed Safari problem by using contentDocument to find the SVG inside the object element, now listening to individual load events, also funkifying inline svgs

--- scripts/funkySVG.js
+++ scripts/funkySVG.js
@@ -6,23 +6,32 @@
  *	author: Olaf Janssen <o.t.a.janssen@gmail.com>
  */
 (function(){
-	window.addEventListener('load',function(){
-			var svgs = [].slice.call(document.querySelectorAll('.funkySVG')); 
-			
-			svgs.forEach(function(e){
-			    var frames = 3,
+	
+	function funkThatSVG(e) {
+				var frames = 3,
 			    	delay = 200,
 			    	maxAngle = 20,
 			    	frame = 0;
 
-			    var paths = [].slice.call(e.getSVGDocument().querySelectorAll("line, path, polygon, rect")); 
+			    var paths = [].slice.call(e.querySelectorAll("line, path, polygon, rect")); 
 			    
 			    function getRandomAngle() {
 			    	return Math.random() * maxAngle - maxAngle/2;
 			    }
 
+			    function getBoundingBox(e) {
+			    	try
+    				{
+			    		return e.getBBox();
+    				}
+    					catch(error)
+    				{
+    					return {left: 0, top: 0, width: 1, height: 1};
+				    } 
+			    }
+
 			    function createAnimationTag(e, multiplier) {
-			    	var values = '', b = e.getBBox();
+			    	var values = '', b = getBoundingBox(e);
 			    	for(var f=0;f<frames;f++) {
 			    		values += (' '+ Math.round(multiplier * getRandomAngle()) +' '+ (b.x + b.width/2) +' '+ (b.y + b.height/2) +';');
 			    	}
@@ -32,17 +41,14 @@
 			    	animate.setAttribute('attributeName', 'transform');
 			    	animate.setAttribute('begin','0s');
 			    	animate.setAttribute('dur', (frames * delay) + 'ms');
-			    	animate.setAttribute('from','0 75 75');
-			    	animate.setAttribute('to','360 75 75');
-			    	animate.setAttribute('values',values);
-			    	// animate.setAttribute('keyTimes','0; 0.3; 1');
+			    	animate.setAttribute('values', values);
 			    	animate.setAttribute('calcMode', 'discrete');
 			    	animate.setAttribute('repeatCount','indefinite');
 			    	e.appendChild(animate);
 			    }
 
 			    function getSurface(element) {
-			    	var bbox = element.getBBox();
+			    	var bbox = getBoundingBox(element);
 			    	return Math.max(bbox.width, bbox.height);
 			    }
 
@@ -57,6 +63,24 @@
 			    paths.forEach(function(e){
 			    	createAnimationTag(e, 1.2 - getSurface(e)/maxSurface);
 			    });
-			});    
+	}
+	
+	document.addEventListener("DOMContentLoaded", function(event) {
+	    var svgs = [].slice.call(document.querySelectorAll('object.funkySVG')); 
+
+		svgs.forEach(function(e){
+			e.addEventListener('load', function(ev){
+				funkThatSVG(e.contentDocument.querySelector('svg'));
+			});
 		});
+  	});
+	
+	window.addEventListener('load',function(){
+			var svgs = [].slice.call(document.querySelectorAll('svg.funkySVG')); 
+			
+			 svgs.forEach(function(e){
+   				funkThatSVG(e);
+			});
+	});
+
  })();
